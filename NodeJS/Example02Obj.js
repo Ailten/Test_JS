6f3668676891675a81cb3530ab6d492b3ae66ba7
Faouzi <faouzi26b@live.fr>
2025-08-02 20:15:44 +0200

    function and obj

---
+++ NodeJS/Example02Obj.js
@@ -0,0 +1,41 @@
+
+// example of object init freely.
+let myObj = {
+	id: 1,
+	name: "Laaster"
+};
+console.log(`name ${myObj.id}: ${myObj.name}`);
+
+// example using class.
+class myClass {
+	constructor(id, name) {
+		this.id = id;
+		this.name = name;
+	}
+}
+let myObjClass = new myClass(2, "Jester");
+console.log(`name ${myObjClass.id}: ${myObjClass.name}`);
+
+// exaple using prototype.
+function myFuncProto(id, name){
+	this.id = id;
+	this.name = name;
+}
+let myObjProto = new myFuncProto(3, "Villa");
+console.log(`name ${myObjProto.id}: ${myObjProto.name}`);
+
+
+// adventage to class : eritage.
+class myClassTwo extends myClass {
+	constructor(id, name, language){
+		super(id, name);
+		this.language = language;
+	}
+}
+let myObjClassTwo = new myClassTwo(4, "Julien", "fr");
+console.log(`name ${myObjClassTwo.id}: ${myObjClassTwo.name}, language : ${myObjClassTwo.language}`);
+
+// adventage to prototype : adding attribute or function on all existing object type out of the constructor declaration.
+myFuncProto.prototype.language = "fr";
+console.log(`${myObjProto.name} language is ${myObjProto.language}`);
+
---
+++ NodeJS/Example03Func.js
@@ -0,0 +1,28 @@
+
+let a = 5;
+let b = 7;
+
+// example of function basic.
+function myFuncAdd(a, b){
+	return a + b;
+}
+console.log(`${a} + ${b} = ${myFuncAdd(a, b)}`);
+
+// example function lambda/ arrow.
+let myLambda = (a, b) => a + b;
+console.log(`${a} + ${b} = ${myLambda(a, b)}`);
+
+// example of async function.
+async function myFuncAsync(msg, wait){
+	await new Promise(() => {
+		setTimeout(() => {}, wait);
+	});
+	console.log(msg);
+}
+myFuncAsync("first launch async 1000", 1000); // suposed to be called in async.
+myFuncAsync("second launch async 500", 500);
+
+// self invoking function (a function call when declarated).
+(function(){
+	console.log("self invoking");
+})();
